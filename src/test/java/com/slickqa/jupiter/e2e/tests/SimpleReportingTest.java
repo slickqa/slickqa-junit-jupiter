966f26f4c389acc00c0d97ede4fc1143d89bb6fe
Jason Corbett <jcorbett@sofi.org>
2019-06-27 14:16:53 -0600

    First E2E Test

--- src/test/java/com/slickqa/jupiter/e2e/tests/SimpleReportingTest.java
+++ src/test/java/com/slickqa/jupiter/e2e/tests/SimpleReportingTest.java
@@ -2,23 +2,36 @@ package com.slickqa.jupiter.e2e.tests;
 
 import com.slickqa.client.SlickClient;
 import com.slickqa.client.SlickClientFactory;
+import com.slickqa.client.model.Result;
+import com.slickqa.jupiter.annotations.SlickMetaData;
+import com.slickqa.jupiter.e2e.SlickJunitRunner;
+import com.slickqa.jupiter.e2e.example.ExampleTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @DisplayName("Simple Reporting Tests")
 public class SimpleReportingTest {
-    private SlickClient slick;
+    private SlickJunitRunner util;
 
     @BeforeEach
     public void setup() throws Exception {
-        slick = SlickClientFactory.getSlickClient(System.getenv("SLICK_E2E_BASEURL"));
+        util = new SlickJunitRunner();
     }
 
     @Test
-    @DisplayName("Slick instance is accessible (precondition).")
-    public void slickUpAndRunning() throws Exception {
-        // this will throw an exception if it can't reach it
-        slick.projects().getList();
+    @DisplayName("Test name reported correctly")
+    public void testNameComesFromSlickMetaData() throws Exception {
+        Method test = ExampleTest.class.getMethod("examplePassTest");
+        Result result = util.runTestMethod(test);
+        assertNotNull(result, "The result from slick should not be null");
+        SlickMetaData info = test.getDeclaredAnnotation(SlickMetaData.class);
+        assertEquals(info.title(), result.getTestcase().getName(), "Test name should be the same as from SlickMetaData");
     }
+
 }
